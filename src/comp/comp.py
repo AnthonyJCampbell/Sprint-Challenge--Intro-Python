7c36a9bd97b96f5ed4718488c1619ea2fc0431e4
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-04 09:12:36 +0200

    2/8 of comprehensions

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -23,49 +23,50 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
-a = []
+print("\nStarts with D:")
+# Loop over `humans` and check if the first letter of the name starts with "D"
+a = [human.name for human in humans if human.name[0] == "D"]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
-print("Ends with e:")
-b = []
+print("\nEnds with e:")
+b = [human.name for human in humans if human.name[-1] == "e"] 
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-print("Starts between C and G, inclusive:")
+print("\nStarts between C and G, inclusive:")
 c = []
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
+print("\nAges plus 10:")
 d = []
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
+print("\nName hyphen age:")
 e = []
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
-print("Names and ages between 27 and 32:")
+print("\nNames and ages between 27 and 32:")
 f = []
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
-print("All names uppercase:")
+print("\nAll names uppercase:")
 g = []
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
+print("\nSquare root of ages:")
 import math
 h = []
 print(h)
